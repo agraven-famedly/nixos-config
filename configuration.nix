4a876c95946ab84b50f800277d439f6d478153c5
Amanda Graven <a.graven@famedly.com>
2026-04-17 14:18:23 +0200

    feat: Enable documentation

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
 		# Include the results of the hardware scan.
 		./hardware-configuration.nix
 		./plymouth.nix
+		./documentation.nix
 	];
 
 	# Bootloader.
---
+++ documentation.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+{
+	environment.systemPackages = [ pkgs.man-pages ];
+
+	documentation = {
+		dev.enable = true;
+		man.generateCaches = true;
+		nixos.includeAllModules = true;
+	};
+}
